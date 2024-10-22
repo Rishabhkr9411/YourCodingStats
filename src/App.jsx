b606f54803578feeae6615b0999a19acb2cc6f8e
Rishabh <rishabhkumar9411@gmail.com>
2024-10-23 03:01:21 +0530

    complete project

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ function App() {
     <Head/>
     <Outlet/>
     {/* <Footer/> */}
+    
     </>
   )
 }
--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -2,7 +2,7 @@ function Contact() {
   return (
     <div>
       <h2>Contact me : +91 7479601671</h2>
-      <h2> Email : rishabhkumarr@gmail.com </h2>
+      <h2> Email : rishabhkumar9411@gmail.com </h2>
     </div>
   );
 }
